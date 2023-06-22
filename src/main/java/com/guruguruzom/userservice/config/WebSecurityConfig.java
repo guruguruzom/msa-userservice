2fc1882bdc12b461b3ef8581e0c3be8c8deac2f1
guruguruzom <guruguruzom@gmail.com>
2023-06-22 21:14:19 +0900

    sequrity 연동

---
+++ src/main/java/com/guruguruzom/userservice/config/WebSecurityConfig.java
@@ -0,0 +1,18 @@
+package com.guruguruzom.userservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //권한 관련 config
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeHttpRequests().antMatchers("/user/**").permitAll();
+    }
+}
--- src/main/java/com/guruguruzom/userservice/controller/UserController.java
+++ src/main/java/com/guruguruzom/userservice/controller/UserController.java
@@ -4,10 +4,13 @@ import com.guruguruzom.userservice.dto.UserDto;
 import com.guruguruzom.userservice.service.UserService;
 import com.guruguruzom.userservice.vo.Greeting;
 import com.guruguruzom.userservice.vo.RequestUser;
+import com.guruguruzom.userservice.vo.ResponseUser;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -35,12 +38,18 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public String createUser(@RequestBody RequestUser user){
+    public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user){
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         UserDto userDto = mapper.map(user, UserDto.class);
         userService.createUser(userDto);
-        return "Create Completed";
+
+        ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
     }
+
+
 }
